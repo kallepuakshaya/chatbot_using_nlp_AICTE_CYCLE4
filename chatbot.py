11734bc8702d0daa2ab2b5bb4762395a6842c4cc
Akshaya <akshayakallepu2002@gmail.com>
2025-08-03 21:38:25 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -14,7 +14,7 @@ nltk.data.path.append(os.path.abspath("nltk_data"))
 nltk.download('punkt')
 
 # Load intents from the JSON file
-file_path = os.path.abspath("C:/Users/PC/Desktop/Implementation of chatbot/intents.json")
+file_path = os.path.abspath("intents.json")
 with open(file_path, "r") as file:
     intents = json.load(file)
 
