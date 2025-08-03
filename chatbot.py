081ea906baef1b47529c0c89c3d2e175c6840d7b
Akshaya <akshayakallepu2002@gmail.com>
2025-08-03 21:30:41 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -8,7 +8,7 @@ import streamlit as st
 import random
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
-
+nltk.download('stopwords')
 ssl._create_default_https_context = ssl._create_unverified_context
 nltk.data.path.append(os.path.abspath("nltk_data"))
 nltk.download('punkt')
